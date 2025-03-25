7f62f8e9dd44b1d22592f6756b47cb2f351ad0ba
gtlab2023 <chenglichengwai2020@gmail.com>
2025-03-25 17:32:42 +0800

    ci: 更新 release.yml 中的 GITHUB_TOKEN 变量 将 GITHUB_TOKEN 从 secrets.RELEASE_TOKEN 更新为 secrets.PAT_TOKEN，以提高安全性并确保与新的权限配置一致。

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,24 +5,24 @@ module.exports = {
       2,
       'always',
       [
-        'feat',     // 新功能
-        'fix',      // 修复bug
-        'docs',     // 文档修改
-        'style',    // 代码格式修改
+        'feat', // 新功能
+        'fix', // 修复bug
+        'docs', // 文档修改
+        'style', // 代码格式修改
         'refactor', // 代码重构
-        'perf',     // 性能优化
-        'test',     // 测试用例修改
-        'chore',    // 其他修改
-        'revert',   // 回滚
-        'build',    // 构建相关
-        'ci'        // CI配置相关
-      ]
+        'perf', // 性能优化
+        'test', // 测试用例修改
+        'chore', // 其他修改
+        'revert', // 回滚
+        'build', // 构建相关
+        'ci', // CI配置相关
+      ],
     ],
-    'type-case': [2, 'always', 'lower'],
+    'type-case': [2, 'always'],
     'type-empty': [2, 'never'],
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
-  }
+    'header-max-length': [2, 'always', 72],
+  },
 };
