b3d2fa30dfdda6b65cc5d03ef651ecdfc9a76d93
gtlab2023 <chenglichengwai2020@gmail.com>
2025-03-25 17:45:35 +0800

    chore: 调整commitlint配置中的header最大长度限制

--- commitlint.config.js
+++ commitlint.config.js
@@ -23,6 +23,6 @@ module.exports = {
     'scope-empty': [1, 'never'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 200],
   },
 };
