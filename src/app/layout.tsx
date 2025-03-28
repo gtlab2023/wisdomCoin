fd0999ff3e08c90fce0667be1229043ebf350f77
gtlab2023 <chenglichengwai2020@gmail.com>
2025-03-28 14:05:01 +0800

    refactor(ui): 移除 Chakra UI 并迁移至 shadcn/ui
    移除 Chakra UI 相关组件和配置，引入 shadcn/ui 作为新的 UI 库。更新了全局样式、按钮组件，并添加了 utils 工具函数。同时，清理了不必要的文件并调整了依赖项。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Provider } from "@/components/ui/provider"
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "WisdomSeed College",
-  description: "web3 college for wisdom",
+  title: 'WisdomSeed College',
+  description: 'web3 college for wisdom',
 };
 
 export default function RootLayout({
@@ -27,7 +26,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-       <Provider>{children}</Provider> 
+        {children}
       </body>
     </html>
   );
